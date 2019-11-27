3a7e5cbcdcbcef6e0350fab7ad4299634c00af7a
Sejuapig <sejuapig@gmail.com>
2019-11-27 16:09:36 +0100

    Test packaging jar

--- src/main/java/canard/esport/recrutement/RecrutementApplication.java
+++ src/main/java/canard/esport/recrutement/RecrutementApplication.java
@@ -3,13 +3,10 @@ package canard.esport.recrutement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import canard.esport.recrutement.services.MainService;
-
 @SpringBootApplication
 public class RecrutementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecrutementApplication.class, args);
-        MainService mainService = new MainService();
 	}
 }
